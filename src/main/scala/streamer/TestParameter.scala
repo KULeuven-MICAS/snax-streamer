16c020dbbbe07ae27aa7a7a97e24b571d16228e0
xiaoling-yi <xiaoling.yi@student.kuleuven.be>
2024-01-09 13:57:39 +0100

    hw: add data reader unit and test

--- src/main/scala/streamer/TestParameter.scala
+++ src/main/scala/streamer/TestParameter.scala
@@ -16,3 +16,15 @@ object UnrollingAddrGenUintTestParameters {
   def addrWidth = 32
 
 }
+
+object DataReaderTestParameters {
+  def dataReaderTcdmPorts = 8
+  def tcdmDataWidth = 64
+  def unrollingFactor = Seq(8, 8)
+  def addrWidth = 32
+  def fifoWidth = 512
+  def elementWidth = 8
+
+  def unrollingDim = unrollingFactor.length
+
+}

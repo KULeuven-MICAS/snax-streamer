0b8dc2f3f969d0b4f24f7ccbf7c6274c76f8e651
Xiaoling Yi <143962462+xiaoling-yi@users.noreply.github.com>
2024-01-12 13:23:23 +0100

    Add address generation units (#1)
    * hw: add temporal address gen unit
    
    * hw: add spatial address gen unit
    
    * hw: fix typo
    
    * Apply suggestions from code review
    
    Co-authored-by: Joren Dumoulin <joren.dumoulin@kuleuven.be>
    Co-authored-by: Ryan Antonio <ryan.albert.antonio@eee.upd.edu.ph>
    
    * hw: rename files
    
    * hw: format
    
    * Apply suggestions from code review
    
    Co-authored-by: Josse Van Delm <josse.vandelm@gmail.com>
    
    * hw: code cleaning...
    
    * restructure PR and add comments (#5)
    
    * variable renaming
    
    * add comments and restructure
    
    * fix error
    
    * fix error
    
    * hw: format
    
    ---------
    
    Co-authored-by: xiaoling-yi <xiaoling.yi@student.kuleuven.be>
    
    * hw: add address finish signal
    
    * hw: add check in test
    
    ---------
    
    Co-authored-by: Joren Dumoulin <joren.dumoulin@kuleuven.be>
    Co-authored-by: Ryan Antonio <ryan.albert.antonio@eee.upd.edu.ph>
    Co-authored-by: Josse Van Delm <josse.vandelm@gmail.com>

---
+++ src/main/scala/streamer/TestParameter.scala
@@ -0,0 +1,17 @@
+package streamer
+
+import chisel3._
+import chisel3.util._
+
+object TemporalAddrGenUnitTestParameters {
+  def loopDim = 3
+  def loopBoundWidth = 8
+  def addrWidth = 32
+
+}
+
+object SpatialAddrGenUnitTestParameters {
+  def loopBounds = Seq(8, 8)
+  def loopDim = loopBounds.length
+  def addrWidth = 32
+}
---
+++ src/test/scala/streamer/SpatialAddrGenUnitTest.scala
@@ -0,0 +1,41 @@
+package streamer
+
+import chisel3._
+import org.scalatest.flatspec.AnyFlatSpec
+import chiseltest._
+import scala.math.BigInt
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.Tag
+
+// manual test for unrolling address generation unit
+// TODO: automated random configuration test and automated results check
+class SpatialAddrGenUnitTest
+    extends AnyFlatSpec
+    with ChiselScalatestTester
+    with Matchers {
+  "DUT" should "pass" in {
+    test(new SpatialAddrGenUnit).withAnnotations(
+      Seq(WriteVcdAnnotation)
+    ) { dut =>
+      dut.clock.step(5)
+      // random config generation
+      val strides =
+        Seq.fill(SpatialAddrGenUnitTestParameters.loopDim)(
+          (
+            scala.util.Random.nextInt(10).U
+          )
+        )
+
+      // sending these configuration to the dut
+      for (i <- 0 until SpatialAddrGenUnitTestParameters.loopDim) {
+        val us = strides(i)
+        dut.io.strides_i(i).poke(us)
+      }
+      dut.io.ptr_i.poke(16.U)
+      dut.io.valid_i.poke(1.B)
+
+      dut.clock.step(5)
+
+    }
+  }
+}
